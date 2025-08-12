ce4a5475092a71b7468e190002fd9cef0d17f21d
ilissonveiga-ai <ilisson.veiga@escola.pr.gov.br>
2025-08-11 22:25:53 -0300

    01

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+js
+const button = document.getElementById('btnMudarCor');
 
+button.addEventListener('click', () => {
+    // Gera uma cor aleatória em hexadecimal
+    const corAleatoria = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = corAleatoria;
+});
